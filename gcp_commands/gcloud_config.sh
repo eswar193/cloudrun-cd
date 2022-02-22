a4ff571e072b696033a4b1f2301f7513131d0972
eswar193 <eswarsaikumar@gmail.com>
2022-02-22 16:44:19 +0000

    modify permission

--- gcp_commands/gcloud_config.sh
+++ gcp_commands/gcloud_config.sh
@@ -6,14 +6,14 @@ PROJECT_ID=$(gcloud projects list | awk '{if(NR==2){print $1}}')
 PROJECT_NUMBER=$(gcloud projects list | awk '{if(NR==2){print $3}}')
 
   # Grant the Cloud Run Admin role to the Cloud Build service account
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member "serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding My First Project \
+  --member "serviceAccount:capable-range-333203@cloudbuild.gserviceaccount.com" \
   --role "roles/run.admin"
-  --role "run.services.update"
+  
 
 # Grant the IAM Service Account User role to the Cloud Build service account on the Cloud Run runtime service account
 gcloud iam service-accounts add-iam-policy-binding \
   $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
-  --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
+  --member="serviceAccount:capable-range-333203@cloudbuild.gserviceaccount.com" \
   --role="roles/iam.serviceAccountUser"
-  --role="run.services.update"
+  
